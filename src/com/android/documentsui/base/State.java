c2cbab4cac929ceb126d98eadcfe591d681c7368
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-02 09:36:45 +0300

    allow specifying which DocumentsProviders to show in the picker UI

--- src/com/android/documentsui/base/State.java
+++ src/com/android/documentsui/base/State.java
@@ -87,6 +87,7 @@ public class State implements android.os.Parcelable {
 
     public boolean allowMultiple;
     public boolean localOnly;
+    public ArrayList<String> allowedAuthorities;
 
     public boolean openableOnly;
     public boolean restrictScopeStorage;
@@ -195,6 +196,7 @@ public class State implements android.os.Parcelable {
         out.writeStringArray(acceptMimes);
         out.writeInt(allowMultiple ? 1 : 0);
         out.writeInt(localOnly ? 1 : 0);
+        out.writeStringList(allowedAuthorities);
         DurableUtils.writeToParcel(out, stack);
         out.writeMap(dirConfigs);
         out.writeList(excludedAuthorities);
@@ -232,6 +234,7 @@ public class State implements android.os.Parcelable {
             state.acceptMimes = in.createStringArray();
             state.allowMultiple = in.readInt() != 0;
             state.localOnly = in.readInt() != 0;
+            state.allowedAuthorities = in.createStringArrayList();
             DurableUtils.readFromParcel(in, state.stack);
             in.readMap(state.dirConfigs, loader);
             in.readList(state.excludedAuthorities, loader);
