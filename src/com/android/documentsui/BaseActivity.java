7249c6d2d77d4dd8c8eabcaf917be54079868f16
Aditya Singh <adityasngh@google.com>
2024-09-09 15:11:53 +0000

    Prevent clickjacking attack in DocsUi.
    * Added permission `HIDE_OVERLAY_WINDOWS` in the Manifest.
    * Set the flag to hide overlay windows to true in BaseActivity and
      ConfirmFragment.
    
    Bug: 233605527
    Test: Manually, see http://b/233605527#comment4
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:5178a952e18ee66f1b3c14796708d0d60058a157)
    Merged-In: I511730856be58cad3e13fa50bfac1e1ee2f5fee0
    Change-Id: I511730856be58cad3e13fa50bfac1e1ee2f5fee0

--- src/com/android/documentsui/BaseActivity.java
+++ src/com/android/documentsui/BaseActivity.java
@@ -172,6 +172,10 @@ public abstract class BaseActivity
         // Record the time when onCreate is invoked for metric.
         mStartTime = new Date().getTime();
 
+        if (SdkLevel.isAtLeastS()) {
+            getWindow().setHideOverlayWindows(true);
+        }
+
         // ToDo Create tool to check resource version before applyStyle for the theme
         // If version code is not match, we should reset overlay package to default,
         // in case Activity continueusly encounter resource not found exception
